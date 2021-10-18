04e6feeeddfa86fcd9402e8714343ed54ef63b9e
karelvanoordt <vanoordtkarel@gmail.com>
2021-10-18 18:33:54 -0500

    Update JavaScript file

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-// IDENTIFIER
-
-// IDs
-// Form = book-form
-// Title = book-title
-// Author = book-author
-
-// Book shelf = book-shelf
-// Book Class: represents a book
-
 class Book {
   constructor(title, author, id) {
     this.title = title;
@@ -16,159 +6,98 @@ class Book {
   }
 }
 
-// UI Class: Handles UI tasks
+class onScreen {
+  static displayBooks() {
+    const books = Store.getBooks();
 
-class UI {
- static displayBooks() {
-    const books = Store.getBooks();    
-    books.forEach((book) => UI.addBooktoList(book));
- }  
+    books.forEach((book) => onScreen.addBookToList(book));
+  }
 
+  static addBookToList(book) {
+    const list = document.querySelector('#book-shelf');
 
- static addBooktoList(book) {
-   const list = document.getElementById('book-shelf'); 
+    const row = document.createElement('li');
 
-   const row = document.createElement('div');
-
-   row.innerHTML = 
-
-    `<p>${book.title}</p>
-     <p>${book.author}</p>
+    row.innerHTML = `
+      <p>${book.title}</p>
+      <p>${book.author}</p>
       <p>${book.id}</p>
-      <a href="#" value="${book.id}" class="delete">REMOVE</a>
-
-    `
-
-
-
-          //STORED 
-  //  `
-  //   <p>${book.title}</p>
-  //   <p>${book.author}</p>
-  //   <p>${book.id}</p>
-  //   <a href="#" class="delete">REMOVE</a>
-  //   `;
-
-
+      <p><a href="#" class="delete">REMOVE</a></p>
+    `;
 
     list.appendChild(row);
- }
+  }
 
- static deleteBook(el) {
-    if (el.classList.contains('delete')) {
+  static deleteBook(el) {
+    if(el.classList.contains('delete')) {
       el.parentElement.parentElement.remove();
     }
- }
+  }
 
- static clearFields() {
-    document.getElementById('book-title').value = '';
-    document.getElementById('book-author').value = '';
-    document.getElementById('book-id').value = '';
- }
+  static clearFields() {
+    document.querySelector('#book-title').value = '';
+    document.querySelector('#book-author').value = '';
+    document.querySelector('#book-id').value = '';
+  }
 }
 
-
-// Store Class: handles local storage (doesnt go away when the page is refreshed)
-
 class Store {
-
-  static getBooks (){
+  static getBooks() {
     let books;
     if(localStorage.getItem('books') === null) {
       books = [];
-    } else { 
+    } else {
       books = JSON.parse(localStorage.getItem('books'));
     }
 
     return books;
   }
 
-  static addBook (book) {
+  static addBook(book) {
     const books = Store.getBooks();
     books.push(book);
     localStorage.setItem('books', JSON.stringify(books));
   }
 
-  static removeBook (id) {
+  static removeBook(id) {
     const books = Store.getBooks();
 
-    // TEMPORARY
-
-    const buttonID = document.querySelector('.delete');
-
-
-
-
-    //
-
     books.forEach((book, index) => {
-      // if (book.id === id) {
-
-
-
-      if (book.id === buttonID) {
-        books.splice(index,1);
+      if(book.id === id) {
+        books.splice(index, 1);
       }
     });
 
     localStorage.setItem('books', JSON.stringify(books));
-
   }
-
 }
 
-// Event: display books
+document.addEventListener('DOMContentLoaded', onScreen.displayBooks);
 
-document.addEventListener('DOMContentLoaded', UI.displayBooks);
+document.querySelector('#booklist-form').addEventListener('submit', (e) => {
+  e.preventDefault();
 
+  const title = document.querySelector('#book-title').value;
+  const author = document.querySelector('#book-author').value;
+  const id = document.querySelector('#book-id').value;
 
-// Event: Add book
+  if(title === '' || author === '' || id === '') {
+    alert('Complete all the fields');
+  } else {
 
-document.getElementById('booklist-form').addEventListener('submit', (e) => {
+    const book = new Book(title, author, id);
 
-// prevent submit
+    onScreen.addBookToList(book);
 
-e.preventDefault();
+    Store.addBook(book);
 
-// get Form values
+    onScreen.clearFields();
+  }
+});
 
-const title = document.getElementById('book-title').value;
-const author = document.getElementById('book-author').value;
-const id = document.getElementById('book-id').value;
+document.querySelector('#book-shelf').addEventListener('click', (e) => {
+  onScreen.deleteBook(e.target);
 
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
-// Validate
-if(title === '' || author === '' || id === '') {
-  alert ('Please complete the fields')
-} else {
-
-  // Instantiate book
-
-  const book = new Book (title, author, id);
-
-  // Add book to display
-
-  UI.addBooktoList(book);
-
-  // Add book to Store
-
-  Store.addBook(book);
-
-  // Clear fields
-  UI.clearFields();
-
-};
-
-// Event: Remove book
-
-document.getElementById('book-shelf').addEventListener('click', (e) => {
-  
-  // Remove book from display
-  UI.deleteBook(e.target);
-
-  // Remove book from Storage
-  // Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
-
-})};
-
-
+  });
